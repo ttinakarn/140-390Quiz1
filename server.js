11732d2b74b735f2a9c24c0c7cbf9f78b0e89fbb
tinakarn <haiiplara@hotmail.com>
2018-09-17 09:02:52 +0700

    Database config

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 var express = require('Express');
 var app = express();
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'www.db4free.net',
+    user     : 'db140390',
+    password : 's140390',
+    database : 'abc123**'
+  });
